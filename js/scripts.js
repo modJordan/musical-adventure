959c0f4d116980e77272eace5688cefb71015b6e
modjordan <mod.jordan@icloud.com>
2023-06-29 13:10:55 -0700

    WIP beepBoop() function progress.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,22 @@ function isEmpty() {
   return false;
 }
 //Business Logic
-
+beepBoop(text) {
+  let numberArray = Array.from(number);
+  let number = parseInt(numberArray);
+  numberArray.forEach(function (num) {
+    for (i = 0; i < numberArray.length; i++)
+      if (num = 1) {
+        console.log("contained 1")
+        return numberArray[i].replace(1, "Beep!");
+      } else {
+        console.log("did not contain 1")
+        return numberArray[i];
+      }
+  })
+  console.log(numberArray);
+  return numberArray;
+}
 
 
 // UI Logic
@@ -18,11 +33,11 @@ function handleFormSubmission(e) {
   const text = document.getElementById("text-input").value;
   document.getElementById("pTag").removeAttribute("hidden")
   document.getElementById("pTag").innerText = beepBoop(text);
-  let boldedPassage = boldPassage(word, passage);
-  if (boldedPassage) {
-    document.querySelector("div#bolded-passage").append(boldedPassage);
+  let beepBoopOutput = beepBoop(text);
+  if (beepBoopOutput) {
+    document.querySelector("p#pOutput").append(beepBoopOutput);
   } else {
-    document.querySelector("div#bolded-passage").innerText = null;
+    document.querySelector("div#Output").innerText = null;
   }
 }
 
