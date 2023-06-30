7d174fee97d33092d6699eb1ca3a8ce9fb802232
modjordan <mod.jordan@icloud.com>
2023-06-30 00:33:50 -0700

    Added fonts and page layout in CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
+window.onload = function () {
+  //Business Logic
+  function beepBoop(text) {
+    text = text.replace(/\s/g, "");
+    const numArray = Array.from(text);
 
-//Business Logic
-function beepBoop(text) {
-  text = text.replace(/\s/g, "");
-  const numArray = Array.from(text);
+    const resultArray = numArray.map(num => {
+      if (num === "1") {
+        return "Beep!";
+      } else if (num === "2") {
+        return "Boop!";
+      } else if (num === "3") {
+        return "Won't you be my neighbor?";
+      } else {
+        return num;
+      }
+    });
+    return resultArray.join(" ");
+  }
 
-  const resultArray = numArray.map(num => {
-    if (num === "1") {
-      return "Beep!"
-    } else if (num === "2") {
-      return "Boop!"
-    } else if (num === "3") {
-      return "Won't you be my neighbor?"
-    } else {
-      return num
-    }
-  });
-  return resultArray.join(" ");
-}
+  // UI Logic
+  function handleFormSubmission(e) {
+    e.preventDefault();
+    const text = document.getElementById("num-input").value;
+    let result = beepBoop(text)
 
-// UI Logic
-function handleFormSubmission(e) {
-  e.preventDefault();
-  const text = document.getElementById("num-input").value;
-  let result = beepBoop(text)
+    document.getElementById("Output").classList.remove("hidden");
+    document.getElementById("outputText").innerText = result;
+  }
 
-  document.getElementById("pTag").removeAttribute("hidden")
-  document.getElementById("pTag").innerText = result;
-  document.querySelector("p#pOutput").innerText = result;
-}
-
-window.addEventListener("load", function () {
   document.querySelector("form#form").addEventListener("submit", handleFormSubmission);
-});
+};
