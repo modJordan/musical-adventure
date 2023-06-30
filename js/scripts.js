0c259e05f8b1604e968df3834ee61806cad9f29f
modjordan <mod.jordan@icloud.com>
2023-06-29 22:11:13 -0700

    changed input field to integer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,46 @@
-//Utility Logic
-function isEmpty() {
-  for (let i = 0; i < arguments.length; i++) {
-    if (arguments[i].trim().length === 0) {
-      return true;
-    }
-  }
-  return false;
-}
+
 //Business Logic
 function beepBoop(text) {
   text = text.replace(/\s/g, "");
   let stringArray = Array.from(text);
-  let resultArray = [];
+  const inputNumber = parseInt(text, 10);
+  let numArray = [];
+  let numResultArray = [];
 
-  stringArray.forEach(function (num) {
-    if (num === "1") {
-      resultArray.push("Beep!")
-    } else if (num === "2") {
-      resultArray.push("Boop!");
-    } else if (num === "3") {
-      resultArray.push("Won't you be my neighbor?")
+  for (let i = 0; i <= inputNumber; i++) {
+    numResultArray.push(i);
+  }
+
+  numResultArray.forEach(function (num) {
+    if (num === 1) {
+      numArray.push("Beep!")
+    } else if (num === 2) {
+      numArray.push("Boop!");
+    } else if (num === 3) {
+      numArray.push("Won't you be my neighbor?")
     } else {
-      resultArray.push(num);
+      numArray.push(num);
     }
   });
-  return resultArray.join(" ")
+  return numArray.join(" ")
+}
+
+function caluclateSpan(text) {
+  numArray = [text]
 }
 
 
 // UI Logic
 function handleFormSubmission(e) {
   e.preventDefault();
-  const text = document.getElementById("text-input").value;
+  const text = document.getElementById("num-input").value;
+  let result = beepBoop(text)
+
   document.getElementById("pTag").removeAttribute("hidden")
-  document.getElementById("pTag").innerText = beepBoop(text);
-  let beepBoopOutput = beepBoop();
-  if (beepBoopOutput) {
-    document.querySelector("p#pOutput").append(beepBoopOutput);
-  } else {
-    document.querySelector("div#Output").innerText = null;
-  }
+  document.getElementById("pTag").innerText = result;
+  document.querySelector("p#pOutput").innerText = result;
 }
 
 window.addEventListener("load", function () {
-  document.querySelector("form#text-input").addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#form").addEventListener("submit", handleFormSubmission);
 });
